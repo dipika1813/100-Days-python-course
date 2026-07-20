4fc4fee53cc72a9f3821949d721b497d689996b7
Dipika1813 <pawardipika24@gmail.com>
2026-07-21 01:17:54 +0530

    new Folder

---
+++ Day 12- For Loop/01_for_loop.py
@@ -0,0 +1,20 @@
+name = "dipika"
+for i in name:
+    print(i,end= " ") 
+
+colors = ["Red", "Green" , "Yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+
+#print numbers from 5 to 9
+for i in range(5, 10, 2):  #(start: stop: step )
+    print(i)
+
+# for num in range(1, 2000, 5):
+#     print(num)
+
+
+  
