b875aa59183ea02d8e64b1ab92453b0dd87e546a
Dipika1813 <pawardipika24@gmail.com>
2026-07-21 20:27:34 +0530

    updates

--- Day 12- For Loop/break.py
+++ Day 12- For Loop/break.py
@@ -1,20 +1,20 @@
-# i = 0
-# while(i <= 5):
-#     if i == 3:
-#         break
-#     else:
-#         print(i)
-#     i += 1
+# break Statement - terminate the loop after break 
+ 
+i = 0
+while(i <= 5):
+    if i == 3:
+        break #terminate
+    else:
+        print(i)
+    i += 1
 
 
-
-# continue statement
+# continue statement - terminate the current iteration & continue execution
 
 i = 0
 while(i <= 5):
     if i == 3:
-        i += 1
-        continue
+        # i += 1
+        continue #skip
     print(i)
-
-    i += 1
+    i += 1  
---
+++ Day 12- For Loop/list_using_loop.py
@@ -0,0 +1,20 @@
+# fruits = ["apple", "mango", "grapes","strawberry"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+names = ["dipika", "vedika", "aditya"]
+
+for naam in names:
+    if naam == "vedika":
+        continue
+    print(naam)
+
+
+# for loop with else
+list = [1,2,3]
+for el in list:
+    print(el)
+    
+else:
+    print("END")
