6ec90b7cd5b79019441a601c0359964d723ea1d3
Dipika1813 <pawardipika24@gmail.com>
2026-07-24 23:05:22 +0530

    new file

---
+++ Day 15- Functions/largest.py
@@ -0,0 +1,17 @@
+def isGreater(a, b):
+    if(a > b):
+        print("First number is greater")
+    else:
+        print("Second number is greater or equal")
+
+a = 9
+b = 8
+isGreater(a, b)
+
+c = 5
+d = 7
+isGreater(c, d)
+
+
+def islesser(a, b):
+    pass
