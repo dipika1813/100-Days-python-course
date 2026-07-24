9f4591ca814f58c3408ce76599ce81c961b81fdc
Dipika1813 <pawardipika24@gmail.com>
2026-07-24 22:20:42 +0530

    new folder

---
+++ Day 13- While Loop/01_loop.py
@@ -0,0 +1,6 @@
+i = int(input("enter a number:"))
+while(i < 3):
+    print(i)
+    i += 1
+
+print("Done with the loop")
---
+++ Day 13- While Loop/02_whileLoop.py
@@ -0,0 +1,14 @@
+i = int(input("enter a number:"))
+while(i <= 28):
+    i = int(input("enter a number:"))
+    print(i)
+print("Done with the loop")
+
+
+count = -5
+while(count > 0):
+    print(count)
+    count = count -1
+#else statement
+else:
+    print("Inside else")
