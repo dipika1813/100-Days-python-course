3504a38c1a7f2979f621d0744f44d49649760aad
Dipika1813 <pawardipika24@gmail.com>
2026-07-28 23:11:22 +0530

    update

--- Day 17- Tuples/tuples.py
+++ Day 17- Tuples/tuples.py
@@ -1,6 +1,12 @@
 # Tuples cannot be modified once it created
 
+t = (1,) #have to use , if you use only one element in tuple
+
+
 tup = ('dipika', 34, 54 ,21)
 print(tup)
 print(type(tup))
 print(tup[2])
+
+tup[3] = 45
+print(tup)
