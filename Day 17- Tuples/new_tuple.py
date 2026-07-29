186a9cda5a7e4a2e2f44ddeca8cc895c0cc536c2
Dipika1813 <pawardipika24@gmail.com>
2026-07-29 22:03:29 +0530

    new update

---
+++ Day 17- Tuples/new_tuple.py
@@ -0,0 +1,3 @@
+tup = (2, 3, 4, 1, 5, 6, 5, 4)
+res = tup.count(4)
+print('Count of 3 in tuple is:', res)
--- Day 17- Tuples/tuple_methods.py
+++ Day 17- Tuples/tuple_methods.py
@@ -7,7 +7,20 @@ countries = tuple(temp)
 print(countries)
 
 
+#Concatanate two tuples:
 countries = ("India", "Italy", "Thailand", "Veitnam")
 countries2 = ("China", "Finland", "Scotland")
 tup = countries + countries2
 print(tup)
+
+#count the number
+tup = (2, 3, 4, 1, 5, 6, 5, 4)
+res = tup.count(4)
+print('Count of 3 in tuple is:', res)
+
+
+#index()method
+tup = (2, 3, 4, 1, 5, 6, 5, 4, 7, 4)
+res = tup.index(4, 5, 9)
+print(res)
+print(len(tup))
