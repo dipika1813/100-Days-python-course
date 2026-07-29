a6424e4aff68cc3da81c14ee0768b48856e1bd18
Dipika1813 <pawardipika24@gmail.com>
2026-07-29 22:16:13 +0530

    new update

--- Day 10- Exercise/exercise.py
+++ Day 10- Exercise/exercise.py
@@ -18,21 +18,24 @@
 
 
 import time
+
 name = input("Enter your name : ")
 name = name.capitalize()
 present_time = time.strftime("%H:%M:%S")
-present_time = int(time.strftime("%H"))
-if (4<=present_time<=12):
-    print(f"Good morning {name}", "it's time : ",present_time)
+hour = int(time.strftime("%H"))
+hour = int(input("enter hour"))
+
+if (hour >0 and  hour < 12):
+    print(f"Good morning {name}")
     print("Thanku sir ")
-elif (12>= present_time <=5):
-    print(f"Good Afternoon {name}", "it's time : ",present_time)
+
+elif (hour > 12 and hour < 17):
+    print(f"Good Afternoon {name}")
     print("Thanku sir ")
-elif (5>= present_time <=8):
-    print(f"Good evening {name}","it's time : ", present_time)
+
+elif (hour >= 17 and hour < 0):
+    print(f" Good Night  {name}")
     print("Thanku sir ")
-else :
-    print(f"Good night {name}","it's time : ",present_time)
-    print("Thanku sir ")
+
 
 
