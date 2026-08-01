c7ee21fd7765733e500a9d52141e14f67e218822
Dipika1813 <pawardipika24@gmail.com>
2026-08-01 23:53:37 +0530

    new file

---
+++ Day 22- Sets/set.py
@@ -0,0 +1,13 @@
+# set are unordered collection of items. set do not conatain duplicate items.
+
+# set = {2, 4, 5, 6, 4}
+# print(set)
+
+
+# info = {"carla", 19, "rahul", 19, 23}
+# print(info)
+
+
+#empty set
+s = set()
+print(type(s))
